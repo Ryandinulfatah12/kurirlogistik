c231af7b90714ae1933a1d31f9a5cca979f1bfbb
Ryandinulfatah12 <ryandfbasketball12@gmail.com>
2021-06-17 08:46:07 +0700

    first commit

---
+++ about-us.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>Temukan Semua Tentang Kami | robialakbar Kuris & Logistik </title>
+	<meta name="description" content="Courier Deprixa V2.5 "/>
+	<meta name="keywords" content="Courier DEPRIXA-Integral Web System" />
+	<meta name="author" content="Jaomweb">	
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	
+		
+	<link rel="stylesheet" href="deprixa_components/hub/css/about-us.css" />	
+	<link rel="stylesheet" href="deprixa_components/hub/css/dSwiper.css" />
+	<link rel="stylesheet" href="deprixa_components/hub/css/global.css" />
+	<link rel="stylesheet" href="deprixa_components/hub/css/bootstrap-mpd.css" />		 
+    <link rel="canonical" href="tracking.php" />
+    
+    <!-- style -->
+    <link href="deprixa_components/content/cssefe4.css" rel="stylesheet"/>   
+    <link href="deprixa_components/styles/track-order.css" rel="stylesheet" />
+
+
+</head>
+   <!-- Menu -->
+<?php include_once "menu.php"; ?>
+    <!-- /Menu -->
+
+<div class="slider-wrapper theme-default">
+   <div id="slider" class="nivoSlider">
+		<img src="deprixa_components/images/slider/about-us.png"  alt="" />
+	</div>		
+</div>
+<main class="slide">
+
+	<div class="fw about-header-bg">
+</div>
+
+<div class="fw">
+<section class="about-us-features">
+<div class="col-md-6 col-sm-12 noPadLeft industryLeading"><header>
+<h2>Industri teknology Kurir dan Logistik</h2>
+</header>
+<p><img src="deprixa_components/images/industry-leading.png" align="left" alt=""></p>
+<p align="Justify"><b>robialakbar Kuris & Logistik</b> hadir dalam rangka menghadapi perkembangan teknologi yang semakin maju terutama di bidang bisnis dan tak luput juga dalam bidang sub bisnis Jasa dan pengiriman, Maka dari itu  robialakbar Kurir dan Logistik hadir dalam rangka untuk memenuhi permintaan masyarakat akan kemudahan dalam mengirimkan barang dari mulai hulu sampai ke hilir yang mana mulai dari permintaaan pengiriman sampai dengan penerimaan di atur sedemikian rupa agar costumer mudah dalam memantau paket yang dikirimkan dan yang akan diterima dan juga costumer bisa dengan mudah dan gembira karena paket yang akan diterimanya sampai ke tujuan dengan baik dan bagi yang mengirimkan akan gembira karena paket yang ia kirimkan bisa dikirimkan dengan baik dan sampai ketujuan, Bagaimana anda tertarik bergabung dengan kami? Dapatkan Beribu kemudahan dan keuntungan dengan bergabung dengan kami secara gratis"</p></center>
+</div>
+<div class="col-md-6">
+<img src="deprixa_components/images/about-us.png" alt="">
+</div>
+</section>
+</div>
+
+</main>
+    <!-- Footer -->
+<?php include_once "footer.php"; ?>
+    <!-- /Footer -->
+	</div>
+	<script src="deprixa_components/hub/scripts/services/services.js"></script>
+	<script src="deprixa_components/hub/scripts/dSwiper.min.js"></script>	
+	<script type='text/javascript' src='deprixa_components/scripts/MPD/integration.js'></script>
+	<script type='text/javascript' src='deprixa_components/scripts/MPD/Common/ga-events.js'></script>
+	    <script src="deprixa_components/bundles/jquery"></script>
+   <script src="deprixa_components/bundles/bootstrap"></script>
+    <script src="deprixa_components/bundles/modernizr"></script>
+    <script src="deprixa_components/scripts/CookieManager.js"></script>
+    <script src="deprixa_components/scripts/MPD/Common/ga-events.js"></script>   
+    <script src="deprixa_components/bundles/jqueryval"></script>
+    <script src="deprixa_components/scripts/tracking.js"></script>
+    <script src="deprixa_components/scripts/placeholder-shim.js"></script>
+    <script src="deprixa_components/scripts/trimFields.js"></script>
+
+	</body>
+
+</html>
